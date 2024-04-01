44ab2832444830801bed610b690f1c0bf2345d36
'JEONwhail' <'green_jeon247'>
2024-04-01 14:01:14 +0900

    0401

--- src/method/quiz/Quiz3.java
+++ src/method/quiz/Quiz3.java
@@ -6,29 +6,25 @@ package method.quiz;
 * 예시) 5,10 => 5+6+7+8+9+10
 */
 public class Quiz3 {
-	
-	//n1부터 n2까지의 합을 구하는 함수 선언 
-	public static int hap(int n1, int n2) {
-		int result = 0;	
-		for(int i=n1; i<=n2; i++) {
-			result = result + i;	
+
+	public static int duham(int n1, int n2) {
+		int result = 0;
+		for (int i = n1; i <= n2; i++) {
+			result = result + i;
 		}
 		return result;
 	}
-	
-	public static void main(String[] args) {	
-		int num1 = 5; 
+
+	public static void main(String[] args) {
+		int num1 = 5;
+
 		int num2 = 10;
-		int sum = hap(num1, num2);	//함수 호출
+
+		int sum = duham(num1, num2);
+
+		System.out.println(num1 + "+" + num2 + "=" + sum);
 		
-		System.out.println(num1 + "부터 " + num2 + "까지의 합은 " + sum + "입니다.");
+
 	}
 
 }
-
-
-//문제풀이
-//매개변수o, 리턴값o
-//두수의 합계를 구하는 반복문 만들기
-//for문안에서 return하면 에러남 
-//for문도 조건문이라 만족하지 못하면 return 못함
--- src/object/quiz/Board.java
+++ src/object/quiz/Board.java
@@ -5,8 +5,8 @@ package object.quiz;
  * 속성: 글번호, 제목, 내용, 작성자
  * */
 public class Board {
-	int no; //글번호 (1)
-	String title; //제목 (안녕하세요) 
-	String content; //내용 (반갑습니다 ~~ )
-	String writer; //작성자 (jiyeon)
+	int no; //글번호 1 
+	String title; //제목
+	String content; //내용
+	String name; //작성자
 }
